f6c9ed3074a46ca2773a3018a437c655e5b58202
user <user@NQOBANI>
2024-04-07 13:42:08 +0200

    Add project files.

---
+++ PoePart1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using RecipeNamespace;
+
+namespace MainNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the name of the recipe:");
+            string recipeName = Console.ReadLine();
+
+            Console.WriteLine("Enter the number of ingredients:");
+            int numIngredients = int.Parse(Console.ReadLine());
+
+            Ingredient[] ingredients = new Ingredient[numIngredients];
+            for (int i = 0; i < numIngredients; i++)
+            {
+                Console.WriteLine($"Enter ingredient {i + 1} details (name, quantity, unit):");
+                string[] ingredientDetails = Console.ReadLine().Split(' ');
+                ingredients[i] = new Ingredient { Name = ingredientDetails[0], Quantity = double.Parse(ingredientDetails[1]), Unit = ingredientDetails[2] };
+            }
+
+            Console.WriteLine("Enter the number of steps:");
+            int numSteps = int.Parse(Console.ReadLine());
+
+            Step[] steps = new Step[numSteps];
+            for (int i = 0; i < numSteps; i++)
+            {
+                Console.WriteLine($"Enter step {i + 1} description:");
+                steps[i] = new Step { Description = Console.ReadLine() };
+            }
+
+            Recipe recipe = new Recipe(recipeName, ingredients, steps);
+
+            recipe.DisplayRecipe();
+        }
+    }
+}
---
+++ PoePart1/recipe.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeNamespace
+{
+    public class Recipe
+    {
+        public string RecipeName { get; set; }
+        public Ingredient[] Ingredients { get; set; }
+        public Step[] Steps { get; set; }
+
+        public Recipe(string name, Ingredient[] ingredients, Step[] steps)
+        {
+            RecipeName = name;
+            Ingredients = ingredients;
+            Steps = steps;
+        }
+
+        public void DisplayRecipe()
+        {
+            Console.WriteLine($"Recipe: {RecipeName}\n");
+
+            Console.WriteLine("Ingredients:");
+            foreach (var ingredient in Ingredients)
+            {
+                Console.WriteLine($"{ingredient.Quantity} {ingredient.Unit} of {ingredient.Name}");
+            }
+
+            Console.WriteLine("\nSteps:");
+            for (int i = 0; i < Steps.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {Steps[i].Description}");
+            }
+        }
+    }
+}
+
